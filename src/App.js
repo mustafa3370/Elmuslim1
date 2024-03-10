476dfd5aef8a79ca00ee92fd382290185a1d8e9e
mustafa3370 <mustafahesham5570@gmail.com>
2024-03-10 22:32:46 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Home from './components/Home/Home';
 import Layout from './components/Layout/Layout';
 import NotFound from './components/NotFound/NotFound';
-const Router = createBrowserRouter([{
-  path:" ",element:<Layout/>,children:[
-    {index:" ",element:<Home/>},
-    {path:'Home',element:<Home/>},
-    { path: "*", element: <NotFound/>}
-  ]}])
+const Router = createBrowserRouter([
+  {path:'/',element:<Layout/>,children:[
+      { path: "home", element: <Home />},
+      { path: "home", element: <Home />},
+      { path: "*", element: <NotFound/>}
+  ]},
+  ])
 function App() {
   return <>
 <RouterProvider router={Router}/>
