b0500aee93a3d7be0ac2e7bb2627c2cd216c912a
mustafa3370 <mustafahesham5570@gmail.com>
2024-03-10 22:16:27 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import Layout from './components/Layout/Layout';
+import NotFound from './components/NotFound/NotFound';
 const Router = createBrowserRouter([{
-  path:'',element:<Layout/>,children:[
-    {index:true,element:<Home/>},
+  path:" ",element:<Layout/>,children:[
+    {index:" ",element:<Home/>},
     {path:'Home',element:<Home/>},
+    { path: "*", element: <NotFound/>}
   ]}])
 function App() {
   return <>
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+function NotFound() {
+
+    return <>
+    <div className="container text-center">
+        <div className="row">
+            <div className="col-md-12">
+                <img src={require("../../photos/404.png")} alt="" className="w-50" />
+            </div>
+        </div>
+    </div>
+    </>
+}
+
+export default NotFound
